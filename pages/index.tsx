1e1ebe4d44bd9fc4ef51c59f54e05ebcaf23090a
PC55 <laryea.enl99@gmail.com>
2022-03-22 12:20:03 +0000

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,23 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+// import Image from 'next/image'
+import { sanityClient, urlFor } from '../sanity.js'
 import Header from '../components/Header'
 
-const Home: NextPage = () => {
+interface Props {
+  posts: [Post];
+}
+
+const Home: NextPage = (props: Props) => {
   return (
-    <div className="">
+    <div className="max-w-7xl mx-auto">
       <Head>
         <title>Meduim Blog</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
 
-      <div>
+      <div className='flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0'>
         <div className='px-10 space-y-5'>
           <h1 className='text-6xl max-w-xl font-serif'>
             <span className='underline decoration-black decoration-4'>Medium</span> {' '}
@@ -21,11 +26,38 @@ const Home: NextPage = () => {
             It's easy and free to post your thinking on any topic and connect with millions of readers.
           </h2>
         </div>
-        <img src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" alt="" />
+
+        <img className='hidden md:inline-flex h-32 lg:h-full' src="https://accountabilitylab.org/wp-content/uploads/2020/03/Medium-logo.png" alt="" />
       </div>
+
+      {/* posts */}
+
 
     </div>
   )
 }
 
 export default Home
+
+export const getServerSideProps = async () => {
+  const query = `
+  *[_type == "post"]{
+    _id,
+    title,
+    author->{
+      name,
+      image
+    },
+    description,
+    mainImage,
+    slug
+  }`;
+
+  const posts = await sanityClient.fetch(query)
+
+  return {
+    props: {
+      posts,
+    }
+  }
+}
