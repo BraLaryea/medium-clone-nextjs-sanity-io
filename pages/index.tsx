b3c4c69b4fd7984cd942b14cd2fa7bd25d1ebabe
PC55 <laryea.enl99@gmail.com>
2022-03-24 14:51:05 +0000

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,14 @@ import Head from 'next/head'
 // import Image from 'next/image'
 import { sanityClient, urlFor } from '../sanity.js'
 import Header from '../components/Header'
+import { Post } from '../typings.js';
+import Link from 'next/link';
 
 interface Props {
   posts: [Post];
 }
 
-const Home: NextPage = (props: Props) => {
+const Home = ({ posts }: Props) => {
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -32,6 +34,22 @@ const Home: NextPage = (props: Props) => {
 
       {/* posts */}
 
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6'>
+        {posts.map(post => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className='border rounded-lg group cursor-pointer overflow-hidden'>
+              <img className='h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out' src={urlFor(post.mainImage).url()} alt="" />
+              <div className='flex justify-between p-5 bg-white'>
+                <div>
+                  <p className='text-lg font-bold'>{post.title}</p>
+                  <p className='text-xs'>{post.description} by {post.author.name}</p>
+                </div>
+                <img className='h-12 w-12 rounded-full' src={urlFor(post.author.image).url()!} alt="" />
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
 
     </div>
   )
